98d7632648abb57ce8dec575a49920a44c8042b8
Ramzesuskan <126349787+Ramzesuskan@users.noreply.github.com>
2025-04-22 23:32:38 +0200

    Update AimLibrary.lua

--- AimLibrary.lua
+++ AimLibrary.lua
@@ -29,12 +29,12 @@ local Library = {
 
     HudRegistry = {};
 
-    FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
+    FontColor = Color3.fromRGB(255, 0, 255);
+    MainColor = Color3.fromRGB(255, 0, 255);
+    BackgroundColor = Color3.fromRGB(222, 173, 222);
     AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
-    RiskColor = Color3.fromRGB(255, 50, 50),
+    OutlineColor = Color3.fromRGB(255, 0, 255);
+    RiskColor = Color3.fromRGB(255, 0, 255),
 
     Black = Color3.new(0, 0, 0);
     Font = Enum.Font.Code,
